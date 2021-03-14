e1da580a1af03008d4098e84bb3f10002353d228
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-15 00:45:02 +0400

    refactor: implement max. min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    let min = array[0];
+    array.forEach(element => {
+      min = (min > element) ? element : min
+    });
+
+    return min;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    let max = array[0];
+    array.forEach(element => {
+      max = (max < element) ? element : max
+    });
+
+    return max;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    let sum = 0;
+    array.forEach(element => {
+      sum += element;
+    });
+
+    return sum / array.length;
+  } else {
+    return 0;
+  }
 }
